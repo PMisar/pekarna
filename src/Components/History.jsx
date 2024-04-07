9ee3e111fbea565f56ab1e9b322230a61a14eae8
PMisar <147728448+PMisar@users.noreply.github.com>
2024-04-07 22:49:57 +0200

    history section edit

--- src/Components/History.jsx
+++ src/Components/History.jsx
@@ -3,69 +3,130 @@ import HistoryImage from "../assets/history.jpg";
 
 const History = () => {
   return (
-    <div className="history-container">
-      <h2>Historie</h2>
-      <div className="history-content">
-        <div className="history-text">
-
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-            elementum, felis a ultricies ultricies, tellus augue efficitur
-            ligula, eget ornare tellus odio vel velit. Mauris vel scelerisque
-            ante. Ut vitae felis dapibus, hendrerit lectus id, auctor urna.
-            Curabitur ex mauris, sagittis in turpis sit amet, ultricies
-            imperdiet felis. Praesent tempus neque vitae erat varius, non
-            molestie velit bibendum. In ac blandit lorem. Sed non imperdiet est.
-            In vestibulum ligula sed purus convallis condimentum.Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Proin elementum, felis
-            a ultricies ultricies, tellus augue efficitur ligula, eget ornare
-            tellus odio vel velit. Mauris vel scelerisque ante. Ut vitae felis
-            dapibus, hendrerit lectus id, auctor urna. Curabitur ex mauris,
-            sagittis in turpis sit amet, ultricies imperdiet felis. Praesent
-            tempus neque vitae erat varius, non molestie velit bibendum. In ac
-            blandit lorem. Sed non imperdiet est. In vestibulum ligula sed purus
-            convallis condimentum.Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Proin elementum, felis a ultricies ultricies,
-            tellus augue efficitur ligula, eget ornare tellus odio vel velit.
-            Mauris vel scelerisque ante. Ut vitae felis dapibus, hendrerit
-            lectus id, auctor urna. Curabitur ex mauris, sagittis in turpis sit
-            amet, ultricies imperdiet felis. Praesent tempus neque vitae erat
-            varius, non molestie velit bibendum. In ac blandit lorem. Sed non
-            imperdiet est. In vestibulum ligula sed purus convallis condimentum.
-          </p>
-          {/* </div> */}
-          {/* <div className="history-image">
+    <>
+      <div className="history-container">
+        <h2>Historie</h2>
+        <div className="history-content">
+          <div className="history-text">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              elementum, felis a ultricies ultricies, tellus augue efficitur
+              ligula, eget ornare tellus odio vel velit. Mauris vel scelerisque
+              ante. Ut vitae felis dapibus, hendrerit lectus id, auctor urna.
+              Curabitur ex mauris, sagittis in turpis sit amet, ultricies
+              imperdiet felis. Praesent tempus neque vitae erat varius, non
+              molestie velit bibendum. In ac blandit lorem. Sed non imperdiet
+              est. In vestibulum ligula sed purus convallis condimentum.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              elementum, felis a ultricies ultricies, tellus augue efficitur
+              ligula, eget ornare tellus odio vel velit. Mauris vel scelerisque
+              ante. Ut vitae felis dapibus, hendrerit lectus id, auctor urna.
+              Curabitur ex mauris, sagittis in turpis sit amet, ultricies
+              imperdiet felis. Praesent tempus neque vitae erat varius, non
+              molestie velit bibendum. In ac blandit lorem. Sed non imperdiet
+              est. In vestibulum ligula sed purus convallis condimentum.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              elementum, felis a ultricies ultricies, tellus augue efficitur
+              ligula, eget ornare tellus odio vel velit. Mauris vel scelerisque
+              ante. Ut vitae felis dapibus, hendrerit lectus id, auctor urna.
+              Curabitur ex mauris, sagittis in turpis sit amet, ultricies
+              imperdiet.
+            </p>
+            {/* </div> */}
+            {/* <div className="history-image">
           <img src={HistoryImage} alt="History Image" />
         </div> */}
-          {/* <div className="history-text"> */}
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-            elementum, felis a ultricies ultricies, tellus augue efficitur
-            ligula, eget ornare tellus odio vel velit. Mauris vel scelerisque
-            ante. Ut vitae felis dapibus, hendrerit lectus id, auctor urna.
-            Curabitur ex mauris, sagittis in turpis sit amet, ultricies
-            imperdiet felis. Praesent tempus neque vitae erat varius, non
-            molestie velit bibendum. In ac blandit lorem. Sed non imperdiet est.
-            In vestibulum ligula sed purus convallis condimentum.Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Proin elementum, felis
-            a ultricies ultricies, tellus augue efficitur ligula, eget ornare
-            tellus odio vel velit. Mauris vel scelerisque ante. Ut vitae felis
-            dapibus, hendrerit lectus id, auctor urna. Curabitur ex mauris,
-            sagittis in turpis sit amet, ultricies imperdiet felis. Praesent
-            tempus neque vitae erat varius, non molestie velit bibendum. In ac
-            blandit lorem. Sed non imperdiet est. In vestibulum ligula sed purus
-            convallis condimentum.Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Proin elementum, felis a ultricies ultricies,
-            tellus augue efficitur ligula, eget ornare tellus odio vel velit.
-            Mauris vel scelerisque ante. Ut vitae felis dapibus, hendrerit
-            lectus id, auctor urna. Curabitur ex mauris, sagittis in turpis sit
-            amet, ultricies imperdiet felis. Praesent tempus neque vitae erat
-            varius, non molestie velit bibendum. In ac blandit lorem. Sed non
-            imperdiet est. In vestibulum ligula sed purus convallis condimentum.
-          </p>
+            {/* <div className="history-text"> */}
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              elementum, felis a ultricies ultricies, tellus augue efficitur
+              ligula, eget ornare tellus odio vel velit. Mauris vel scelerisque
+              ante. Ut vitae felis dapibus, hendrerit lectus id, auctor urna.
+              Curabitur ex mauris, sagittis in turpis sit amet, ultricies
+              imperdiet felis. Praesent tempus neque vitae erat varius, non
+              molestie velit bibendum. In ac blandit lorem. Sed non imperdiet
+              est. In vestibulum ligula sed purus convallis condimentum.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              elementum, felis a ultricies ultricies, tellus augue efficitur
+              ligula, eget ornare tellus odio vel velit. Mauris vel scelerisque
+              ante. Ut vitae felis dapibus, hendrerit lectus id, auctor urna.
+              Curabitur ex mauris, sagittis in turpis sit amet, ultricies
+              imperdiet felis. Praesent tempus neque vitae erat varius, non
+              molestie velit bibendum. In ac blandit lorem. Sed non imperdiet
+              est. In vestibulum ligula sed purus convallis condimentum.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              elementum, felis a ultricies ultricies, tellus augue efficitur
+              ligula, eget ornare tellus odio vel velit. Mauris vel scelerisque
+              ante. Ut vitae felis dapibus, hendrerit lectus id, auctor urna.
+              Curabitur ex mauris, sagittis in turpis sit amet, ultricies
+              imperdiet.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className="history-container2">
+        <h2>Historie</h2>
+        <div className="history-content2">
+          <div className="left-column">
+            <div className="history-image2">
+              <img src={HistoryImage} alt="History Image" />
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              elementum, felis a ultricies ultricies, tellus augue efficitur
+              ligula, eget ornare tellus odio vel velit. Mauris vel scelerisque
+              ante. Ut vitae felis dapibus, hendrerit lectus id, auctor urna.
+              Curabitur ex mauris, sagittis in turpis sit amet, ultricies
+              imperdiet felis. Praesent tempus neque vitae erat varius, non
+              molestie velit bibendum. In ac blandit lorem. Sed non imperdiet
+              est. In vestibulum ligula sed purus convallis condimentum.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              elementum, felis a ultricies ultricies, tellus augue efficitur
+              ligula, eget ornare tellus odio vel velit. Mauris vel scelerisque
+              ante. Ut vitae felis dapibus, hendrerit lectus id, auctor urna.
+              Curabitur ex mauris, sagittis in turpis sit amet, ultricies
+              imperdiet felis. Praesent tempus neque vitae erat varius, non
+              molestie velit bibendum. In ac blandit lorem. Sed non imperdiet
+              est. In vestibulum ligula sed purus convallis condimentum.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              elementum, felis a ultricies ultricies, tellus augue efficitur
+              ligula, eget ornare tellus odio vel velit. Mauris vel scelerisque
+              ante. Ut vitae felis dapibus, hendrerit lectus id, auctor urna.
+              Curabitur ex mauris, sagittis in turpis sit amet, ultricies
+              imperdiet felis. Praesent tempus neque vitae erat varius, non
+              molestie velit bibendum. In ac blandit lorem. Sed non imperdiet
+              est. In vestibulum ligula sed purus convallis condimentum. Proin
+              elementum, felis a ultricies ultricies, tellus augue efficitur
+              ligula, eget ornare tellus odio vel velit. Mauris vel scelerisque
+              ante.
+            </p>
+          </div>
+
+          <div className="right-column">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              elementum, felis a ultricies ultricies, tellus augue efficitur
+              ligula, eget ornare tellus odio vel velit. Mauris vel scelerisque
+              ante. Ut vitae felis dapibus, hendrerit lectus id, auctor urna.
+              Curabitur ex mauris, sagittis in turpis sit amet, ultricies
+              imperdiet felis. Praesent tempus neque vitae erat varius, non
+              molestie velit bibendum. In ac blandit lorem. Sed non imperdiet
+              est. In vestibulum ligula sed purus convallis condimentum.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              elementum, felis a ultricies ultricies, tellus augue efficitur
+              ligula, eget ornare tellus odio vel velit. Mauris vel scelerisque
+              ante. Ut vitae felis dapibus, hendrerit lectus id, auctor urna.
+              Curabitur ex mauris, sagittis in turpis sit amet, ultricies
+              imperdiet felis. Praesent tempus neque vitae erat varius, non
+              molestie velit bibendum. In ac blandit lorem. Sed non imperdiet
+              est. In vestibulum ligula sed purus convallis condimentum.Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,13 +1,66 @@
+// import React, { useState } from 'react';
+
+// // Modal Component
+// const Modal = ({ onClose, productName }) => {
+//   return (
+//     <div className="modal">
+//       <div className="modal-content">
+//         <span className="close" onClick={onClose}>&times;</span>
+//         <h2>{productName}</h2>
+//         <p>Details about {productName}...</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// // Products Component
+// const Products = () => {
+//   const [showModal, setShowModal] = useState(false);
+//   const [selectedProduct, setSelectedProduct] = useState('');
+
+//   // Function to handle click on box
+//   const handleBoxClick = (productName) => {
+//     setSelectedProduct(productName);
+//     setShowModal(true);
+//   };
+
+//   // Function to close the modal
+//   const handleCloseModal = () => {
+//     setShowModal(false);
+//   };
+
+//   return (
+//     <div className="products-container" id="products">
+//       <div className="centered-content">
+//         <h2>Naše výroba</h2>
+//         <p>*Klikni na obrázek pro více informací</p>
+//       </div>
+//       <div className="boxes-container">
+//         <div className="box" onClick={() => handleBoxClick('Chléb velký')}>Chléb velký</div>
+//         <div className="box" onClick={() => handleBoxClick('Chléb malý')}>Chléb malý</div>
+//         <div className="box" onClick={() => handleBoxClick('Houska obyčejná')}>Houska obyčejná</div>
+//         <div className="box" onClick={() => handleBoxClick('Houska cereální')}>Houska cereální</div>
+//         <div className="box" onClick={() => handleBoxClick('Dalamánek')}>Dalamánek</div>
+//         <div className="box" onClick={() => handleBoxClick('Vánočka')}>Vánočka</div>
+//         <div className="box" onClick={() => handleBoxClick('Koláč')}>Koláč</div>
+//       </div>
+//       {showModal && <Modal onClose={handleCloseModal} productName={selectedProduct} />}
+//     </div>
+//   );
+// };
+
+// export default Products;
+
 import React, { useState } from 'react';
 
 // Modal Component
-const Modal = ({ onClose, productName }) => {
+const Modal = ({ onClose, productName, productDetails }) => {
   return (
     <div className="modal">
       <div className="modal-content">
         <span className="close" onClick={onClose}>&times;</span>
         <h2>{productName}</h2>
-        <p>Details about {productName}...</p>
+        <p>{productDetails}</p>
       </div>
     </div>
   );
@@ -17,16 +70,28 @@ const Modal = ({ onClose, productName }) => {
 const Products = () => {
   const [showModal, setShowModal] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState('');
+  const [productDetails, setProductDetails] = useState('');
 
   // Function to handle click on box
   const handleBoxClick = (productName) => {
     setSelectedProduct(productName);
+    setProductDetails(productsData[productName]);
     setShowModal(true);
   };
 
   // Function to close the modal
   const handleCloseModal = () => {
     setShowModal(false);
+  };
+
+  const productsData = {
+    'Chléb velký': 'Velký klasický chléb, ideální pro celou rodinu. Obsahuje pšeničnou mouku, vodu, droždí, sůl. Alergeny: obsahuje pšeničnou mouku.',
+    'Chléb malý': 'Malý klasický chléb, vhodný pro jednotlivce. Obsahuje pšeničnou mouku, vodu, droždí, sůl. Alergeny: obsahuje pšeničnou mouku.',
+    'Houska obyčejná': 'Obyčejná houska, ideální pro snídani nebo svačinu. Obsahuje pšeničnou mouku, vodu, droždí, sůl. Alergeny: obsahuje pšeničnou mouku.',
+    'Houska cereální': 'Houska s cereáliemi, bohatá na vlákninu a chutná. Obsahuje pšeničnou mouku, vodu, droždí, sůl, cereálie. Alergeny: obsahuje pšeničnou mouku, cereálie.',
+    'Dalamánek': 'Sladký výrobek s náplní, ideální k čaji nebo kávě. Obsahuje pšeničnou mouku, cukr, máslo, droždí, sůl, náplň. Alergeny: obsahuje pšeničnou mouku, máslo.',
+    'Vánočka': 'Tradiční vánoční pečivo, vhodné k oslavě svátků. Obsahuje pšeničnou mouku, cukr, máslo, droždí, sůl, sušené ovoce. Alergeny: obsahuje pšeničnou mouku, máslo, sušené ovoce.',
+    'Koláč': 'Koláč s ovocnou náplní, ideální k odpolednímu čaji. Obsahuje pšeničnou mouku, cukr, máslo, droždí, sůl, ovocná náplň. Alergeny: obsahuje pšeničnou mouku, máslo.'
   };
 
   return (
@@ -36,15 +101,11 @@ const Products = () => {
         <p>*Klikni na obrázek pro více informací</p>
       </div>
       <div className="boxes-container">
-        <div className="box" onClick={() => handleBoxClick('Chléb velký')}>Chléb velký</div>
-        <div className="box" onClick={() => handleBoxClick('Chléb malý')}>Chléb malý</div>
-        <div className="box" onClick={() => handleBoxClick('Houska obyčejná')}>Houska obyčejná</div>
-        <div className="box" onClick={() => handleBoxClick('Houska cereální')}>Houska cereální</div>
-        <div className="box" onClick={() => handleBoxClick('Dalamánek')}>Dalamánek</div>
-        <div className="box" onClick={() => handleBoxClick('Vánočka')}>Vánočka</div>
-        <div className="box" onClick={() => handleBoxClick('Koláč')}>Koláč</div>
+        {Object.keys(productsData).map(productName => (
+          <div className="box" key={productName} onClick={() => handleBoxClick(productName)}>{productName}</div>
+        ))}
       </div>
-      {showModal && <Modal onClose={handleCloseModal} productName={selectedProduct} />}
+      {showModal && <Modal onClose={handleCloseModal} productName={selectedProduct} productDetails={productDetails} />}
     </div>
   );
 };
