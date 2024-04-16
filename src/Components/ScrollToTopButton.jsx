f89825fc8c62b3fe820696273486a1a80d30df65
PMisar <147728448+PMisar@users.noreply.github.com>
2024-04-16 20:40:25 +0200

    transition time duration change

--- src/Components/ScrollToTopButton.jsx
+++ src/Components/ScrollToTopButton.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ArrowUp from "../assets/arrow-up.png";
 
 const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 150) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
   return (
-    <button className="scroll-to-top-button" onClick={scrollToTop}>
-      <img src={ArrowUp} alt="Scroll to Top" />
-    </button>
+    <>
+      {isVisible && (
+        <button className="scroll-to-top-button" onClick={scrollToTop}>
+          <img src={ArrowUp} alt="Scroll to Top" />
+        </button>
+      )}
+    </>
   );
 };
 
