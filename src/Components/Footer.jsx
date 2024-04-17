257a520fd593ab2bf05c9979e562312e89d9083b
PMisar <147728448+PMisar@users.noreply.github.com>
2024-04-17 19:35:22 +0200

    navbar fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,20 +1,3 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-
-// const Footer = () => {
-//   return (
-//     <div className="footer-container">
-//       <p>© 2024 Pekárna Větrný Jeníkov s.r.o. IČO: 29363870, DIČ: CZ12341234</p>
-//       <div>
-//         <Link to="/privacy-policy"><button>Zásady zpracování osobních údajů</button></Link>
-//         <Link to="/cookies-policy"><button>Podmínky používání cookies</button></Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Footer;
-
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -24,8 +7,12 @@ const Footer = () => {
       <p>© 2024 Pekárna Větrný Jeníkov s.r.o. IČO: 29363870, DIČ: CZ12341234</p>
       {/* <p className="link">Vytvořil <a href="https://pavelmisar.netlify.app/" target="_blank" rel="noopener noreferrer">Pavel Misař</a></p> */}
       <div>
-        <Link to="/privacy-policy"><button>Zásady zpracování osobních údajů</button></Link>
-        <Link to="/cookies-policy"><button>Podmínky používání cookies</button></Link>
+        <Link to="/privacy-policy">
+          <button>Zásady zpracování osobních údajů</button>
+        </Link>
+        <Link to="/cookies-policy">
+          <button>Podmínky používání cookies</button>
+        </Link>
       </div>
     </div>
   );
