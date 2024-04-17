13702d59fc1d9860085ed952d8f8049e2ce5ab0d
PMisar <147728448+PMisar@users.noreply.github.com>
2024-04-17 13:30:36 +0200

    css edit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,3 +1,20 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// const Footer = () => {
+//   return (
+//     <div className="footer-container">
+//       <p>© 2024 Pekárna Větrný Jeníkov s.r.o. IČO: 29363870, DIČ: CZ12341234</p>
+//       <div>
+//         <Link to="/privacy-policy"><button>Zásady zpracování osobních údajů</button></Link>
+//         <Link to="/cookies-policy"><button>Podmínky používání cookies</button></Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Footer;
+
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -5,6 +22,7 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <p>© 2024 Pekárna Větrný Jeníkov s.r.o. IČO: 29363870, DIČ: CZ12341234</p>
+      <p className="link">Vytvořil <a href="https://pavelmisar.netlify.app/" target="_blank" rel="noopener noreferrer">Pavel Misař</a></p>
       <div>
         <Link to="/privacy-policy"><button>Zásady zpracování osobních údajů</button></Link>
         <Link to="/cookies-policy"><button>Podmínky používání cookies</button></Link>
