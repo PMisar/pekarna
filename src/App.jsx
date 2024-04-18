34019d2c3eab7e4428f41387881e0989bd1f16f7
PMisar <147728448+PMisar@users.noreply.github.com>
2024-04-18 14:50:05 +0200

    finish line

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,12 @@
-// import React from "react";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Header from "./Components/Header";
-// import About from "./Components/About";
-// import History from "./Components/History";
-// import Products from "./Components/Products";
-// import JobOffer from "./Components/JobOffer";
-// import Retailers from "./Components/Retailers";
-// import Contact from "./Components/Contact";
-// import Footer from "./Components/Footer";
-// import ScrollToTopButton from "./Components/ScrollToTopButton";
-// import PrivacyPolicy from "./Pages/PrivacyPolicyPage";
-// import CookiesPolicy from "./Pages/CookiesPage";
-// import "./App.css";
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Header />
-//         <Routes>
-//           <Route path="/privacy-policy" component={PrivacyPolicy} />
-//           <Route path="/cookies-policy" component={CookiesPolicy} />
-//           <Route path="/" exact />
-//         </Routes>
-//         <About />
-//         <History />
-//         <Products />
-//         <JobOffer />
-//         <Retailers />
-//         <Contact />
-//         <Footer />
-//         <ScrollToTopButton />
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
 import About from "./Components/About";
 import History from "./Components/History";
 import Products from "./Components/Products";
@@ -56,17 +22,34 @@ import "./App.css";
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/cookies-policy" element={<CookiesPolicy />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-        <Footer />
-        <ScrollToTopButton />
-      </div>
+      <AppContent />
     </Router>
+  );
+}
+
+function AppContent() {
+  const location = useLocation();
+
+  // Function to determine if Navbar should be hidden based on current route
+  const shouldHideNavbar = () => {
+    return (
+      location.pathname === "/privacy-policy" ||
+      location.pathname === "/cookies-policy"
+    );
+  };
+
+  return (
+    <div className="App">
+      {!shouldHideNavbar() && <Navbar />}
+      <Header />
+      <Routes>
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/cookies-policy" element={<CookiesPolicy />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+      <ScrollToTopButton />
+    </div>
   );
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import Navbar from "./Navbar";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import BannerImage from "../assets/header-image.jpg";
 
 const Header = () => {
   return (
     <>
-      <Navbar />
       <div className="home-banner-container" id="header">
-          <img src={BannerImage} alt="" />
-          <div className="header-text">
-            <span className="large-text">Pekárna</span>
-            <br />
-            <span className="small-text">Větrný Jeníkov</span>
-          </div>
+        <img src={BannerImage} alt="" />
+        <Link to="/" className="header-text">
+          <span className="large-text">Pekárna</span>
+          <br />
+          <span className="small-text">Větrný Jeníkov</span>
+        </Link>
       </div>
-      </>
+    </>
   );
 };
 
